b049e7b3a467d0ae91cd3f035f11a36a584886b2
Nathan Ringo <remexre@gmail.com>
2019-04-22 04:04:59 -0500

    Fixes benchmarks to be more reasonable.

--- libs/ecs/benches/component_store.rs
+++ libs/ecs/benches/component_store.rs
@@ -1,100 +1,75 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use ecs::{Component, ComponentStore};
+use criterion::{criterion_group, criterion_main, Criterion};
+use ecs::{Component, ComponentStore, Entity};
+use rand::random;
 
-#[derive(Debug)]
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
 struct ComponentZST;
 impl Component for ComponentZST {}
 
-#[derive(Debug, Eq, PartialEq)]
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
 struct ComponentWord(usize);
 impl Component for ComponentWord {}
 
-#[derive(Debug, Eq, PartialEq)]
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
 struct ComponentStr(&'static str);
 impl Component for ComponentStr {}
 
+fn set_up_component_store<T: Component + Copy>(
+    component: T,
+    present: bool,
+) -> (ComponentStore, Entity) {
+    // This stymies both optimizations where component store memory is lazily allocated and where
+    // it behaves differently with very few entities.
+
+    let mut cs = ComponentStore::new();
+    for _ in 0..5000 {
+        let e = cs.new_entity();
+        if random() {
+            cs.set_component(e, component);
+        }
+    }
+    let e = cs.new_entity();
+    if present {
+        cs.set_component(e, component);
+    }
+    for _ in 0..5000 {
+        let e = cs.new_entity();
+        if random() {
+            cs.set_component(e, component);
+        }
+    }
+
+    (cs, e)
+}
+
+fn bench_getter<T: Component + Copy + PartialEq>(
+    c: &mut Criterion,
+    component: T,
+    name: &str,
+    present: bool,
+) {
+    c.bench_function(name, move |b| {
+        let (cs, e) = set_up_component_store(component, present);
+        if present {
+            b.iter(|| assert_eq!(cs.get_component(e), Some(&component)))
+        } else {
+            b.iter(|| assert_eq!(cs.get_component::<T>(e), None))
+        }
+    });
+}
+
+fn bench_getters<T: Component + Copy + PartialEq>(c: &mut Criterion, component: T, name: &str) {
+    bench_getter(c, component, &format!("get {} (present)", name), true);
+    bench_getter(c, component, &format!("get {} (not present)", name), false);
+}
+
 fn component_store(c: &mut Criterion) {
-    c.bench_function("get ZST (present)", |b| {
-        let mut cs = ComponentStore::new();
-        for _ in 0..5000 {
-            cs.new_entity();
-        }
-        let e = cs.new_entity();
-        for _ in 0..5000 {
-            cs.new_entity();
-        }
-        cs.set_component(e, ComponentZST);
-        b.iter(|| cs.get_component::<ComponentZST>(e))
-    });
-
-    c.bench_function("get ZST (not present)", |b| {
-        let mut cs = ComponentStore::new();
-        for _ in 0..5000 {
-            cs.new_entity();
-        }
-        let e = cs.new_entity();
-        for _ in 0..5000 {
-            cs.new_entity();
-        }
-        b.iter(|| cs.get_component::<ComponentZST>(e))
-    });
-
-    c.bench_function("get word-sized (present)", |b| {
-        let mut cs = ComponentStore::new();
-        for _ in 0..5000 {
-            cs.new_entity();
-        }
-        let e = cs.new_entity();
-        for _ in 0..5000 {
-            cs.new_entity();
-        }
-        cs.set_component(e, ComponentWord(12345));
-        b.iter(|| cs.get_component::<ComponentWord>(e))
-    });
-
-    c.bench_function("get word-sized (not present)", |b| {
-        let mut cs = ComponentStore::new();
-        for _ in 0..5000 {
-            cs.new_entity();
-        }
-        let e = cs.new_entity();
-        for _ in 0..5000 {
-            cs.new_entity();
-        }
-        b.iter(|| {
-            black_box(cs.get_component::<ComponentWord>(e));
-        })
-    });
-
-    c.bench_function(
-        "get word-sized with null pointer optimization (present)",
-        |b| {
-            let mut cs = ComponentStore::new();
-            for _ in 0..5000 {
-                cs.new_entity();
-            }
-            let e = cs.new_entity();
-            for _ in 0..5000 {
-                cs.new_entity();
-            }
-            cs.set_component(e, ComponentStr("12345"));
-            b.iter(|| cs.get_component::<ComponentStr>(e))
-        },
-    );
-
-    c.bench_function(
-        "get word-sized with null pointer optimization (not present)",
-        |b| {
-            let mut cs = ComponentStore::new();
-            for _ in 0..5000 {
-                cs.new_entity();
-            }
-            let e = cs.new_entity();
-            for _ in 0..5000 {
-                cs.new_entity();
-            }
-            b.iter(|| cs.get_component::<ComponentStr>(e))
-        },
+    bench_getters(c, ComponentZST, "ZST");
+    bench_getters(c, ComponentWord(12345), "word-sized");
+    bench_getters(
+        c,
+        ComponentStr("12345"),
+        "word-sized with null pointer optimization",
     );
 
     c.bench_function("get not-present, set, get present; word-sized", |b| {
