c4ce67308d9bf87b5d27546a5d0bfeff3edd1c8f
Nathan Ringo <remexre@gmail.com>
2019-05-03 12:41:59 -0500

    ...

--- libs/assets/src/cache.rs
+++ libs/assets/src/cache.rs
@@ -1,7 +1,46 @@
 use crate::{Model, Texture};
-use std::{collections::HashMap, path::PathBuf, sync::RwLock};
+use antidote::RwLock;
+use libremexre::errors::Result;
+use std::{
+    collections::HashMap,
+    path::{Path, PathBuf},
+    sync::Arc,
+};
 
-lazy_static::lazy_static! {
-    static ref MODELS: RwLock<HashMap<PathBuf, Model>> = RwLock::new(HashMap::new());
-    static ref TEXTURES: RwLock<HashMap<PathBuf, Texture>> = RwLock::new(HashMap::new());
+macro_rules! cache {
+    ($ty:ident, $name:ident, $name_str:literal) => {
+        mod $name {
+            use super::*;
+
+            lazy_static::lazy_static! {
+                static ref CACHE: RwLock<HashMap<PathBuf, Arc<$ty>>> = RwLock::new(HashMap::new());
+            }
+
+            impl $ty {
+                #[doc = "Clears the "]
+                #[doc = $name_str]
+                #[doc = " cache."]
+                pub fn clear_cache() {
+                    CACHE.write().clear()
+                }
+
+                #[doc = "Loads a "]
+                #[doc = $name_str]
+                #[doc = " by path. The "]
+                #[doc = $name_str]
+                #[doc = " is cached; use `clear_cache()` to clear it."]
+                pub fn load_from(path: &Path) -> Result<Arc<$ty>> {
+                    if let Some(val) = CACHE.read().get(path) {
+                        Ok(val.clone())
+                    } else {
+                        let cache = CACHE.write();
+                        unimplemented!("{:?}", *cache)
+                    }
+                }
+            }
+        }
+    };
 }
+
+cache!(Model, model, "model");
+cache!(Texture, texture, "texture");
