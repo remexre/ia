964cda8f0e9d404a68db95660294b0c742ff658b
Nathan Ringo <remexre@gmail.com>
2019-05-10 04:34:15 -0500

    ...

--- libs/assets/src/program.rs
+++ libs/assets/src/program.rs
@@ -1,8 +1,15 @@
 use crate::Asset;
 use derive_more::From;
 use ecstasy::Component;
-use serde::{Deserialize, Serialize};
-use std::{error::Error, sync::Arc};
+use serde::{
+    de::{Error as DeError, Unexpected},
+    Deserialize, Deserializer, Serialize, Serializer,
+};
+use std::{
+    error::Error,
+    fmt::{Formatter, Result as FmtResult},
+    sync::Arc,
+};
 
 /// A program.
 #[derive(Component, Debug, Deserialize, From, Serialize)]
@@ -30,7 +37,48 @@ pub struct ProgramInner {
 pub struct ProgramSafetyPromise(());
 
 impl ProgramSafetyPromise {
+    const STR: &'static str = "I promise this is safe!";
+
     pub unsafe fn i_promise() -> ProgramSafetyPromise {
         ProgramSafetyPromise(())
+    }
+}
+
+impl<'de> Deserialize<'de> for ProgramSafetyPromise {
+    fn deserialize<D>(deserializer: D) -> Result<ProgramSafetyPromise, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        struct Visitor;
+
+        impl<'de> serde::de::Visitor<'de> for Visitor {
+            type Value = ProgramSafetyPromise;
+
+            fn expecting(&self, formatter: &mut Formatter) -> FmtResult {
+                formatter.write_str("the string \"I promise this is safe!\"")
+            }
+
+            fn visit_str<E: DeError>(self, value: &str) -> Result<ProgramSafetyPromise, E> {
+                if value == ProgramSafetyPromise::STR {
+                    Ok(ProgramSafetyPromise(()))
+                } else {
+                    Err(E::invalid_value(
+                        Unexpected::Str(value),
+                        &ProgramSafetyPromise::STR,
+                    ))
+                }
+            }
+        }
+
+        deserializer.deserialize_str(Visitor)
+    }
+}
+
+impl Serialize for ProgramSafetyPromise {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        serializer.serialize_str(ProgramSafetyPromise::STR)
     }
 }
